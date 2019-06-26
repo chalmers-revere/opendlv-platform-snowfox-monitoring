e9cbf7466d772f2133d20830b67b199f34bcb652
Christian Berger <christian.berger@gu.se>
2019-06-27 00:08:26 +0200

    collector Snowfox/Brick
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ collectd/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+/opt/collectd_cuda.sh
+
+if [ -d /mnt/proc ]; then
+  umount /proc
+  mount -o bind /mnt/proc /proc
+fi
+
+if [ -z "$@" ]; then
+  exec /usr/sbin/collectd -C /etc/collectd/collectd.conf -f
+else
+  exec $@
+fi
